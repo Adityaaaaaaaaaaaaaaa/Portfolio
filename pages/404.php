e159257397d72583895455376eec4bf51efd0eb6
Aditya <171126285+Adityaaaaaaaaaaaaaaa@users.noreply.github.com>
2024-12-03 00:36:47 +0400

    404 page

--- pages/404.php
+++ pages/404.php
@@ -1,1 +1,33 @@
-404
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>404</title>
+	<link rel="stylesheet">
+	<link rel="stylesheet" href="../css/404.css">
+	<link rel="stylesheet" href="../css/main.css">
+	<script src="../js/dark_mode.js"></script>
+
+</head>
+
+<body>
+	<?php include('../php/templates/header.php'); ?>
+
+	<div class="box">
+		<div class="text">
+			<div>ERROR</div>
+			<h1>404</h1>
+			<hr>
+			<div>Page Not Found</div>
+		</div>
+
+		<div class="astronaut">
+			<img src="https://images.vexels.com/media/users/3/152639/isolated/preview/506b575739e90613428cdb399175e2c8-space-astronaut-cartoon-by-vexels.png" alt="" class="src">
+		</div>
+	</div>
+
+	<?php include('../php/templates/footer.php'); ?>
+	<script src="../js/404.js"></script>
+</body>
+</html>
